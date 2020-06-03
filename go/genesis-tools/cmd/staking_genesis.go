e3d8729cc2d5419c06dbbbba8e2321618fe1eb43
Reuven V. Gonzales <reuven@oasislabs.com>
2020-06-03 14:22:27 -0700

    properly label staking params
    Previously it was called consensus params which is incorrect

--- go/genesis-tools/cmd/staking_genesis.go
+++ go/genesis-tools/cmd/staking_genesis.go
@@ -19,7 +19,7 @@ const (
 	cfgTotalSupply             = "staking.total_supply"
 	cfgPrecisionConstant       = "staking.precision_constant"
 	cfgEntitiesDirectoryPaths  = "staking.entities_dir"
-	cfgConsensusParametersPath = "staking.consensus_params"
+	cfgStakingParametersPath   = "staking.params"
 	cfgDefaultFundingAmount    = "staking.default_funding"
 	cfgDefaultSelfEscrowAmount = "staking.default_self_escrow"
 	cfgOutputPath              = "output-path"
@@ -49,7 +49,7 @@ func doStakingGenesis(cmd *cobra.Command, args []string) {
 		TotalSupply:             viper.GetInt64(cfgTotalSupply),
 		PrecisionConstant:       viper.GetInt64(cfgPrecisionConstant),
 		EntitiesDirectoryPaths:  viper.GetStringSlice(cfgEntitiesDirectoryPaths),
-		ConsensusParametersPath: viper.GetString(cfgConsensusParametersPath),
+		ConsensusParametersPath: viper.GetString(cfgStakingParametersPath),
 		DefaultFundingAmount:    viper.GetInt64(cfgDefaultFundingAmount),
 		DefaultSelfEscrowAmount: viper.GetInt64(cfgDefaultSelfEscrowAmount),
 	}
@@ -103,7 +103,7 @@ func RegisterStakingGenesisCmd(parentCmd *cobra.Command) {
 	stakingGenesisFlags.Int64(cfgPrecisionConstant, defaultPrecisionConstant,
 		"the precision constant for a single token defaults to 10^18")
 	stakingGenesisFlags.StringSlice(cfgEntitiesDirectoryPaths, []string{}, "a directory entities")
-	stakingGenesisFlags.String(cfgConsensusParametersPath, "",
+	stakingGenesisFlags.String(cfgStakingParametersPath, "",
 		"a consensus params json file (defaults to using ./consensus_params.json relative to entities directory)")
 	stakingGenesisFlags.Int64(cfgDefaultFundingAmount, 0, "Default funding amount")
 	stakingGenesisFlags.Int64(cfgDefaultSelfEscrowAmount, defaultSelfEscrowAmount, "Default amount to self escrow")
